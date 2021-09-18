49b544b0fe5edb7d418a152f71cd4fdc4ea0db0e
dashingsoocooldude <88321624+dashingsoocooldude@users.noreply.github.com>
2021-09-18 17:15:51 +0100

    Delete js directory

--- js/age.js
+++
@@ -1,6 +0,0 @@
-let ageEl = document.getElementById("age");
-
-setInterval(() => {
-	let time = dayjs().diff(dayjs(1044801960000), 'year', true);
-	ageEl.innerText = time.toString().substring(0, 12);
-}, 50);
